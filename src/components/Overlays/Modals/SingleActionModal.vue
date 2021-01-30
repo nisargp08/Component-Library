cccba06f716b6f913bf7dad9a057eb63b3c4ccb3
Nisarg Patel <nisargp42@gmail.com>
2021-01-30 16:27:11 -0500

    Overlay :: Modal :: Single Action :: Done

--- src/components/Overlays/Modals/SingleActionModal.vue
+++ src/components/Overlays/Modals/SingleActionModal.vue
@@ -1,6 +1,8 @@
 <template>
   <div>
-    <button class="btn" @click="isOpen = !isOpen">Open Simple Modal</button>
+    <button class="btn" @click="isOpen = !isOpen">
+      Open Single Action Modal
+    </button>
     <div class="modal" :class="{ 'is-open': isOpen }">
       <transition
         mode="out-in"
@@ -29,7 +31,7 @@
           />
         </svg>
       </button>
-      <div class="modal-content modal-sm">
+      <div class="modal-content modal-xs">
         <div
           v-for="(value, index) in 1"
           :key="index"
@@ -39,36 +41,32 @@
           <div class="header">
             <div class="icon">
               <svg
-                class="h-6 w-6"
-                x-description="Heroicon name: exclamation"
                 xmlns="http://www.w3.org/2000/svg"
+                class="h-6 w-6"
                 fill="none"
                 viewBox="0 0 24 24"
                 stroke="currentColor"
-                aria-hidden="true"
               >
                 <path
                   stroke-linecap="round"
                   stroke-linejoin="round"
                   stroke-width="2"
-                  d="M12 9v2m0 4h.01m-6.938 4h13.856c1.54 0 2.502-1.667 1.732-3L13.732 4c-.77-1.333-2.694-1.333-3.464 0L3.34 16c-.77 1.333.192 3 1.732 3z"
-                ></path>
+                  d="M5 13l4 4L19 7"
+                />
               </svg>
             </div>
             <div class="text">
-              <h3 class="text-title">Deactivate account</h3>
+              <h3 class="text-title">Payment Successful</h3>
               <div class="text-data">
                 <p>
-                  Are you sure you want to deactivate your account? All of your
-                  data will be permanently removed. This action cannot be
-                  undone.
+                  Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                  Nostrum voluptas, blanditiis adipisci excepturi ex maiores.
                 </p>
+                <button class="btn btn-block btn-indigo action-btn mt-4">
+                  Go back to dashboard
+                </button>
               </div>
             </div>
-          </div>
-          <div class="footer">
-            <button class="btn btn-red action-btn">Deactivate</button>
-            <button class="btn" @click="isOpen = false">Cancel</button>
           </div>
         </div>
       </div>
@@ -174,12 +172,7 @@ $bg-tint: rgb(10, 10, 10);
   border-radius: 0.375rem;
   margin: 0 1rem;
   .header {
-    padding: 1.5rem 1rem 1.25rem 1rem;
-    @media (min-width: 768px) {
-      display: flex;
-      align-items: flex-start;
-      padding: 1.5rem 1.5rem 1rem 1.5rem;
-    }
+    padding: 1rem;
   }
   .icon {
     display: flex;
@@ -188,8 +181,8 @@ $bg-tint: rgb(10, 10, 10);
     justify-content: center;
     width: 3rem;
     height: 3rem;
-    color: rgba(220, 38, 38, 1);
-    background-color: rgba(254, 226, 226, 1);
+    color: rgba(#059669, 1);
+    background-color: rgba(#d1fae5, 1);
     border-radius: 9999px;
     margin-left: auto;
     margin-right: auto;
@@ -200,19 +193,11 @@ $bg-tint: rgb(10, 10, 10);
   }
   .text {
     text-align: center;
-    @media (min-width: 768px) {
-      text-align: left;
-      margin-left: 1rem;
-    }
     .text-title {
       font-size: 1.125rem;
-      //   letter-spacing: -0.025rem;
       line-height: 1.5rem;
       font-weight: 600;
       margin-bottom: 0;
-      @media (min-width: 768px) {
-        margin-top: 0;
-      }
     }
     .text-data {
       font-size: 0.875rem;
@@ -221,44 +206,20 @@ $bg-tint: rgb(10, 10, 10);
       padding-top: 0.5rem;
     }
   }
-  .footer {
-    display: flex;
-    align-items: center;
-    flex-direction: column;
-    row-gap: 0.75rem;
-    background-color: $text-gray-50;
-    padding: 0.75rem 1rem;
-    border-radius: 0.375rem;
-    @media (min-width: 768px) {
-      flex-direction: row;
-      flex-direction: row-reverse;
-      padding-left: 1.5rem;
-      padding-right: 1.5rem;
-      .action-btn {
-        margin-left: 0.5rem;
-      }
-    }
-    .btn {
-      width: 100%;
-      font-size: 1rem;
-      font-weight: 600;
-      @media (min-width: 768px) {
-        width: auto;
-        font-size: 0.875rem;
-      }
-    }
-  }
-  .btn-red {
+  .btn-indigo {
     color: white;
-    background-color: rgba(220, 38, 38, 1);
-    border: 1px solid rgba(220, 38, 38, 1);
+    background-color: rgba(#6366f1, 1);
+    border: 1px solid rgba(#6366f1, 1);
     &:hover {
-      background-color: darken($color: rgba(220, 38, 38, 1), $amount: 3%);
+      background-color: darken($color: rgba(#6366f1, 1), $amount: 3%);
     }
     &:focus {
       outline: none;
-      box-shadow: 0 0 0 0.25rem rgba($color: rgba(220, 38, 38, 1), $alpha: 0.5);
+      box-shadow: 0 0 0 0.25rem rgba($color: rgba(#6366f1, 1), $alpha: 0.5);
     }
+  }
+  .btn-block {
+    width: 100%;
   }
 }
 </style>
--- src/views/Overlays/Modals.vue
+++ src/views/Overlays/Modals.vue
@@ -12,6 +12,16 @@
                 <simple-modal></simple-modal>
             </template>
         </component-box>
+        <!-- Centered Modal with single action-->
+        <component-box>
+            <template v-slot:header>
+                Centered with single action
+                <span class="badge badge-red ml-1">requires js</span>
+            </template>
+            <template v-slot:body>
+                <single-action-modal></single-action-modal>
+            </template>
+        </component-box>
     </section>
   </div>
 </template>
@@ -20,11 +30,13 @@
 import ComponentBox from "@general/ComponentBox.vue";
 //Modals
 import SimpleModal from "@modals/SimpleModal.vue";
+import SingleActionModal from "@modals/SingleActionModal.vue";
 
 export default {
   components: {
     ComponentBox,
     SimpleModal,
+    SingleActionModal,
   },
 };
 </script>
